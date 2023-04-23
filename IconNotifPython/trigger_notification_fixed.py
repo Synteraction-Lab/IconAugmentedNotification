e133e9287be085fdf650c203c65843f3aa9cad92
janakanuwan <45715403+janakanuwan@users.noreply.github.com>
2023-04-23 09:32:19 +0200

    enable to support walking

--- IconNotifPython/trigger_notification_fixed.py
+++ IconNotifPython/trigger_notification_fixed.py
@@ -9,7 +9,7 @@ DEVICE_IP = device_config.get_device_ip()
 DISPLAY_URL = 'http://' + DEVICE_IP + ':8080/displays/10/'
 NOTIFICATION_URL = 'http://' + DEVICE_IP + ':8080/notifiers/12/'
 
-NOTIFICATION_DISPLAY_SECONDS = 5
+NOTIFICATION_DISPLAY_SECONDS = 4
 
 MAX_RETRY_ATTEMPT = 4
 
@@ -60,6 +60,29 @@ def send_factor_data(factor):
             {"subheading": "Doctor's appointment in 2 hours"},
             {"subheading": "  2 hrs", "image": "#FF00FF7D img_doctor4"},
         ]
+    elif factor == 'w': # walking
+        data = [
+            # {"subheading": "Presentation   12 pm"},
+            # {"subheading": "  12 pm", "image": "#FF00FF7D img_presentation2"},
+            {"subheading": "Car   5 min"},
+            {"subheading": "  5 min", "image": "#FF00FF7D img_car4"},
+            # {"subheading": "Movie   Sunday"},
+            # {"subheading": "  Sunday", "image": "#FF00FF7D img_movie4"},
+            {"subheading": "Delivery   3 d"},
+            {"subheading": "  3 d", "image": "#FF00FF7D img_delivery4"},
+            # {"subheading": "Birthday   John"},
+            # {"subheading": "  John", "image": "#FF00FF7D img_birthday4"},
+            {"subheading": "Weight lifting   40 min"},
+            {"subheading": "  40 min", "image": "#FF00FF7D img_exercise1"},
+            {"subheading": "Valentine's day   2 d"},
+            {"subheading": "  2 d", "image": "##FF00FF7D img_valentine_day4"},
+            # {"subheading": "Backup computer   tonight"},
+            # {"subheading": "  tonight", "image": "#FF00FF7D img_backup_computer4"},
+            {"subheading": "Driving license   renew"},
+            {"subheading": "  renew", "image": "#FF00FF7D img_licence1"},
+            {"subheading": "Milk and eggs   Walmart"},
+            {"subheading": "  Walmart", "image": "#FF00FF7D img_milk_eggs4"},
+        ]
 
     counter += 1
     if counter >= len(data):
@@ -76,7 +99,7 @@ def clear_display_data():
 
 _res = ''
 while _res != 'n':
-    _res = input("Continue? (0/3/t/i/n/f/r)")
+    _res = input("Continue? (n/t/i/c/f/d/b/w/r)")
 
     if _res == 't':
         send_sample_text_display_data()
@@ -86,15 +109,15 @@ while _res != 'n':
         send_sample_image_display_data()
         utilities.sleep_seconds(NOTIFICATION_DISPLAY_SECONDS)
 
-    if _res == '3':
-        send_sample_text_display_data()
-        utilities.sleep_seconds(3)
 
-    if _res == 'f' or _res == 'd' or _res == 'b' :
+    if _res == 'f' or _res == 'd' or _res == 'b' or _res == 'w':
         send_factor_data(_res)
         utilities.sleep_seconds(NOTIFICATION_DISPLAY_SECONDS)
 
     if _res == 'r':
         counter = -1
+        clear_display_data()
 
-    clear_display_data()
+    # clear_display_data()
+    if _res == 'c':
+        clear_display_data()
